3e39b7783c3a4b13511789ae7b2bb43956e11046
song0152 <songshuxun5@gmail.com>
2025-08-08 21:58:21 -0400

    Update

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,19 +1,6 @@
 export default [
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:", "http:"],
-          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          "upgrade-insecure-requests": null,
-        },
-      },
-    },
-  },
+  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
