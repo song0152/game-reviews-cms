4f7499c0b25aeb0c10ae1d348fa51d913b80f74e
song0152 <songshuxun5@gmail.com>
2025-08-08 21:38:22 -0400

    chore: allow Cloudinary in CSP (img-src/media-src)

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,22 @@
 export default [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:", "http:"],
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "upgrade-insecure-requests": null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
