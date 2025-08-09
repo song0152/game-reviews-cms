bee95eb601f04b8f97b9d336dc39ecc0887e98c2
song0152 <songshuxun5@gmail.com>
2025-08-08 21:23:49 -0400

    fix: bind host 0.0.0.0 and use PORT on Render

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,8 @@
 export default ({ env }) => ({
   url: env('PUBLIC_URL'),
   proxy: true,
+  host: '0.0.0.0',           
+  port: env.int('PORT', 1337),  
   app: {
     keys: env.array('APP_KEYS'),
   },
